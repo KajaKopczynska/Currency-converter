5a17cf6940774b0c6536c7b8355b4050f99f661a
Kaja Kopczynska <kaja.a.kopczynska@gmail.com>
2023-02-14 19:32:54 +0100

    result to return

--- js/form.js
+++ js/form.js
@@ -1,40 +1,49 @@
 {
-  const formElement = document.querySelector(".js-form");
-  const amountElement = document.querySelector(".js-amount");
-  const inputCurrencyElement = document.querySelector(".js-inputCurrency");
-  const outputCurrencyElement = document.querySelector(".js-outputCurrency");
-  const resultElement = document.querySelector(".js-result");
 
-  const pln = 1;
-  const eur = 4.71;
-  const usd = 4.33;
+
+  const calculateResult = (inputCurrency, outputCurrency, amount) => {
+    const pln = 1;
+    const eur = 4.71;
+    const usd = 4.33;
+
+
+    switch (inputCurrency) {
+      case "PLN":
+        if (outputCurrency === "PLN") return amount * 1;
+        else if (outputCurrency === "EUR") return (amount * pln) / eur;
+        else if (outputCurrency === "USD") return (amount * pln) / usd;
+        break;
+      case "EUR":
+        if (outputCurrency === "EUR") return amount * 1;
+        else if (outputCurrency === "USD") return (amount * eur) / usd;
+        else if (outputCurrency === "PLN") return (amount * eur) / pln;
+        break;
+      case "USD":
+        if (outputCurrency === "USD") return amount * 1;
+        else if (outputCurrency === "EUR") return (amount * usd) / eur;
+        else if (outputCurrency === "PLN") return (amount * usd) / pln;
+        break;
+    }
+  }
+
+
+  const formElement = document.querySelector(".js-form");
+
 
   formElement.addEventListener("submit", (event) => {
     event.preventDefault();
+
+
+    const amountElement = document.querySelector(".js-amount");
+    const inputCurrencyElement = document.querySelector(".js-inputCurrency");
+    const outputCurrencyElement = document.querySelector(".js-outputCurrency");
+    const resultElement = document.querySelector(".js-result");
 
     const amount = amountElement.value;
     const inputCurrency = inputCurrencyElement.value;
     const outputCurrency = outputCurrencyElement.value;
 
-
-    switch (inputCurrency) {
-      case "PLN":
-        if (outputCurrency === "PLN") result = amount * 1;
-        else if (outputCurrency === "EUR") result = (amount * pln) / eur;
-        else if (outputCurrency === "USD") result = (amount * pln) / usd;
-        break;
-      case "EUR":
-        if (outputCurrency === "EUR") result = amount * 1;
-        else if (outputCurrency === "USD") result = (amount * eur) / usd;
-        else if (outputCurrency === "PLN") result = (amount * eur) / pln;
-        break;
-      case "USD":
-        if (outputCurrency === "USD") result = amount * 1;
-        else if (outputCurrency === "EUR") result = (amount * usd) / eur;
-        else if (outputCurrency === "PLN") result = (amount * usd) / pln;
-        break;
-    }
-
+    const result = calculateResult(inputCurrency, outputCurrency, amount);
 
     resultElement.innerText = `${result.toFixed(2)} ${outputCurrency}`;
 
